7442ae6984d75302f07035882846a81759878fd7
wanyi <408629610@qq.com>
2020-12-05 12:13:40 +0000

    12-6 生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('users','UserController',['only'=>['show','update','edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
