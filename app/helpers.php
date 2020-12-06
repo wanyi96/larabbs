1366070dc1f12c2bb29bdf493ac778c9e004b0c3
wanyi <408629610@qq.com>
2020-12-06 22:05:50 +0000

    12-7 用户创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -10,3 +10,9 @@ function category_nav_active($category_id)
     //active_class()方法，如果传参满足参数1，则会返回参数二'active'，否则返回参数3''
     return active_class((if_route('categories.show') && if_route_param('category',$category_id)));
 }
+
+function make_excerpt($value,$lenth = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/',' ',strip_tags($value)));
+    return Str::limit($excerpt,$lenth);
+}
