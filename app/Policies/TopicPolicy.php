851f9935333b975a7dab951c26ecb5ed21060ce6
wanyi <408629610@qq.com>
2020-12-07 22:20:30 +0000

    12-8 用户可以删除自己发的话题

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -7,14 +7,14 @@ use App\Models\Topic;
 
 class TopicPolicy extends Policy
 {
+    //默认传当前用户对象为参数1；判断帖子的user_id是否是当前用户
     public function update(User $user, Topic $topic)
     {
-        return $topic->user_id == $user->id;
-        // return true;
+        return $user->isAuthOf($topic);
     }
 
     public function destroy(User $user, Topic $topic)
     {
-        return true;
+        return $user->isAuthOf($topic);
     }
 }
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -43,16 +43,23 @@
               {!! $topic->body !!}
             </div>
 
-
+            @can('update',$topic)
             <div class="operate">
               <hr>
               <a href="{{ route('topics.edit',$topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
-                <li class="far fa-edit"></li>编辑
+                <i class="far fa-edit"></i>编辑
               </a>
-              <a href="#" class="btn btn-outline-secondary btn-sm" role="button">
-                <li class="far fa-trash-alt"></li>删除
+              <form action="{{ route('topics.destroy',$topic->id) }}" method="post"
+                  style="display: inline-block;"
+                  onsubmit="return confirm('您确定要删除吗？');">
+                  {{ csrf_field() }}
+                  {{ method_field('DELETE') }}
+                  <button type="submit" class="btn btn-outline-secondary btn-sm">
+                    <i class="far fa-trash-alt"></i>删除
+                  </button>
               </a>
             </div>
+            @endcan
 
           </div>
         </div>
