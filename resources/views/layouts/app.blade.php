653437e990d006e91fed04b678c37b59631840af
wanyi <408629610@qq.com>
2020-12-07 18:36:50 +0000

    12-7 添加simditor富文本编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,10 @@
   <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
 
   <!-- Styles -->
-<link href="{{ mix('css/app.css')}}" rel="stylesheet">
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+  @yield('styles')
+
 </head>
 
 <body>
@@ -34,6 +37,9 @@
 
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
+
+  @yield('scripts')
+
 </body>
 
 </html>
