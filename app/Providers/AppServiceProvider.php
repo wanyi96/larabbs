91b2a81cefee7558887a2b696b36f3484211ad01
wanyi <408629610@qq.com>
2020-12-08 13:18:14 +0000

    12-9 使用代码生成器构成reply骨架，并且数据填充

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
 	{
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
+		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
         Schema::defaultStringLength(191); //add fixed sql,队列执行报错，似乎是mysql版本过低，上网查了下，补充这行代码
         //
