05590ac29ca977d4e4761dc51348b8ed7ff72eb1
wanyi <408629610@qq.com>
2020-12-09 00:51:35 +0000

    12-9 用户可以发表评论，并过滤xss攻击

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -16,4 +16,9 @@ class Reply extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function scopeRecent($query)
+{
+    return $query->orderBy('id', 'desc');
+}
+
 }
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -11,7 +11,18 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        //
+        //过滤表单提交内容，HTMLPurifier白名单中未指定的html属性，css属性，html标签都会被过滤
+        //参数1是传入的数据，参数二是要过滤的规则，在配置文件中配置好
+        $reply->content = clean($reply->content,'user_topic_body');
+    }
+
+    public function created(Reply $reply)
+    {
+        //评论创建之后，在评论对应的帖子上将字段reply_count评论数量+1。
+        // $reply->topic->increment('reply_count',1);
+        //创建成功后计算本话题下评论总数，然后再对其reply_count字段赋值
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
     }
 
     public function updating(Reply $reply)
