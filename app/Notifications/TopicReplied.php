ea4ce3560539a6573944a00ad5bb11d3862a053e
wanyi <408629610@qq.com>
2020-12-13 12:03:59 +0000

    12-13 邮件通知，以及队列版邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use App\Models\Reply;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -29,7 +29,7 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
         //开启通知的频道
-        return ['database'];
+        return ['database','mail'];
     }
 
     //该方法接收一个$notifiable实例参数并返回一个普通的php数组
@@ -59,10 +59,15 @@ class TopicReplied extends Notification
      */
     public function toMail($notifiable)
     {
+        // $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+        // return (new MailMessage)
+        //             ->line('你的话题有新回复！')
+        //             ->action('查看回复', $url);
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->line('你的话题有新回复！')
+                    ->action('查看回复', $url);
     }
 
 }
