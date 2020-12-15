ca31bb9f15636ed15949313009e88aa4cfd82e5c
wanyi <408629610@qq.com>
2020-12-15 03:52:01 +0000

    12-15 站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
       由 <a href="http://whispering-brook-20656.herokuapp.com/" target="_blank">Wanyi</a> 设计和编码<span style="color: #e27575; font-size: 14px;"> ❤</span>
     </p>
 
-    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+    <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
   </div>
 </footer>
