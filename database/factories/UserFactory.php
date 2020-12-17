d7b6e3371581d8d3bbf15744de851fd72549f43d
wanyi <408629610@qq.com>
2020-12-17 04:56:59 +0000

    12-17 用户最后登录时间，定期由redis存储到数据库中

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -35,7 +35,8 @@ $factory->define(User::class, function (Faker $faker) use ($avatars) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'avatar' => $faker->randomElement($avatars),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => 'password', // password
         'remember_token' => Str::random(10),
         'introduction' =>$faker->sentence(),
         'created_at' => $date_time,
